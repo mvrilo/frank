260151835f59e18297ee3ed90af307731abde4c4
Murilo Santana <mvrilo@gmail.com>
2010-10-31 20:22:02 -0200

    v1.1.0; added HEAD response, changed the jsmin for the uglifyjs for a better compression, dependencies included and partially jslinted

--- test/frank-test.js
+++ test/frank-test.js
@@ -4,12 +4,13 @@ var f = require('../lib/frank'),
 
 get('/').write('<h1>it works!</h1>', 'html').end();
 get('/write').write(' TEXT PLAIN :D ').end();
-get('/minify').write('var opa = function() { \ndocument.write("ok") \n};', 'text/javascript').minify(3).end();
 get('/send').send('./frank-test.html').end();
+get('/write_minify').write('var opa = function() { \ndocument.write("ok") \n};', 'text/javascript').minify().end();
+get('/send_minify').send('jquery.js', 'text/javascript').minify().end();
 get('/function').then(function(req, res) {
 	res.writeHead(200, {'content-type':'text/plain'});
 	res.end('FUNCTION OK');
 }).end();
-get('not found').write('ok').end();
+get('not found').write('not found, dude').end();
 
 run(8888);
