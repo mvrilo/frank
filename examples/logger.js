34a9f434e48070e61b2e99c1dbcff41cab098605
Murilo Santana <mvrilo@gmail.com>
2011-12-11 12:44:01 -0200

    defining variables fix and using `errorHandler` middleware on logger.js example

--- examples/logger.js
+++ examples/logger.js
@@ -4,5 +4,8 @@ var http = require('../lib/frank').createServer();
 http.get('/okay').send('okay');
 http.get('/test').send('test');
 
-http.use(connect.logger('tiny'));
+http.use(connect.errorHandler());
+
+// http.use(connect.logger('tiny'));
+http.logger('tiny');
 http.listen(8000);
